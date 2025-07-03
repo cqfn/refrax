2beda9c0ddfdd9aa79d417276b33d607ea6e5fbc
h1alexbel <h1alexbelx@gmail.com>
2025-07-03 14:58:02 +0200

    feat(#2): passes

--- internal/aibolit/aibolit_response.go
+++ internal/aibolit/aibolit_response.go
@@ -1,5 +1,10 @@
 package aibolit
 
+import (
+	"regexp"
+	"strings"
+)
+
 type AibolitResponse struct {
 	data string
 }
@@ -9,5 +14,12 @@ func NewAibolitResponse(data string) *AibolitResponse {
 }
 
 func (r *AibolitResponse) Sanitized() string {
-	return ""
+	complaint := regexp.MustCompile(`^[^:]+\.java\[\d+\]: .+`)
+	var complaints []string
+	for _, line := range strings.Split(r.data, "\n") {
+		if complaint.MatchString(line) {
+			complaints = append(complaints, line)
+		}
+	}
+	return strings.Join(complaints, "\n")
 }
