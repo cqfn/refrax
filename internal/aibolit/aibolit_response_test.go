0af25a7b8c75a44841c3fe8cfe1b2cc48a93ac84
h1alexbel <h1alexbelx@gmail.com>
2025-07-03 14:47:25 +0200

    feat(#2): correct test case

--- internal/aibolit/aibolit_response_test.go
+++ internal/aibolit/aibolit_response_test.go
@@ -1,15 +1,23 @@
 package aibolit
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAibolitResponse_Sanitizes_Response(t *testing.T) {
-	expected := "refrax/Foo.java[50]: Non final class (P24: 0.20)"
-	sanitized := NewAibolitResponse(
-		"ignore: []\nShow pattern with the largest contribution to Cognitive Complexity\nrefrax/Foo.java[50]: Non final class (P24: 0.20)",
-	).Sanitized()
-	assert.Equal(t, expected, sanitized)
+	expected := []string {
+		"x/Foo.java[50]: Non final class (P24: 0.20)",
+		"x/Foo.java[476]: Private static method (P25: 7.50)",
+		"x/Foo.java[471]: String concat (P17: 1.60)",
+	}
+	lines := []string {
+		"ignore: []",
+		"Show pattern with the largest contribution to Cognitive Complexity",
+	}
+	lines = append(lines, expected...)
+	sanitized := NewAibolitResponse(strings.Join(lines, "\n")).Sanitized()
+	assert.Equal(t, strings.Join(expected, "\n"), sanitized)
 }
